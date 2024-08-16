1a9f4a02b975a4552341af7d18180b9339f5a90f
Ivan Botero <ivan.botero@protonmail.ch>
2024-08-16 12:25:15 -0500

    ✨Added "dr" VPC

--- providers.tf
+++ providers.tf
@@ -3,12 +3,23 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "5.63.0"
+      configuration_aliases = [
+        aws.main, aws.dr
+      ]
     }
   }
 }
 
 provider "aws" {
+  alias      = "main"
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = var.AWS_REGION
+}
+
+provider "aws" {
+  alias      = "dr"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = "us-west-2"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,15 @@
 locals {
-  cidr-main = "10.0.0.0/16"
+  cidr-main = "10.10.0.0/16"
   azs-main  = ["us-east-1a", "us-east-1b"]
+  cidr-dr   = "10.20.0.0/16"
+  azs-dr    = ["us-west-2a", "us-west-2b"]
 }
 
 module "vpc-main" {
+
+  providers = {
+    aws = aws.main
+  }
 
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.13.0"
@@ -14,6 +20,27 @@ module "vpc-main" {
   azs             = local.azs-main
   public_subnets  = [cidrsubnet(local.cidr-main, 8, 1), cidrsubnet(local.cidr-main, 8, 2)]
   private_subnets = [cidrsubnet(local.cidr-main, 8, 101), cidrsubnet(local.cidr-main, 8, 102)]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+}
+
+module "vpc-dr" {
+
+  providers = {
+    aws = aws.dr
+  }
+
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.13.0"
+
+  name = "dr"
+  cidr = local.cidr-dr
+
+  azs             = local.azs-dr
+  public_subnets  = [cidrsubnet(local.cidr-dr, 8, 1), cidrsubnet(local.cidr-dr, 8, 2)]
+  private_subnets = [cidrsubnet(local.cidr-dr, 8, 101), cidrsubnet(local.cidr-dr, 8, 102)]
 
   enable_dns_hostnames = true
   enable_dns_support   = true
