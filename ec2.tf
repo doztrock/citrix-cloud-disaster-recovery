462db0f28f04b44486e87563bf83a12f8e780009
Ivan Botero <ivan.botero@protonmail.ch>
2024-08-16 22:06:08 -0500

    🚀Added Cloud Connectors and Golden Image server

--- ec2.tf
+++ ec2.tf
@@ -12,6 +12,9 @@ data "aws_ami" "ami-dr-windows" {
   owners      = ["amazon"]
 }
 
+#
+# Domain Controller (Main)
+#
 module "ec2-main-dc" {
 
   providers = {
@@ -39,6 +42,69 @@ module "ec2-main-dc" {
 
 }
 
+#
+# Cloud Connector (Main)
+#
+module "ec2-main-cc" {
+
+  providers = {
+    aws = aws.main
+  }
+
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.1"
+
+  name          = "CCPD01"
+  instance_type = "t3.medium"
+  ami           = data.aws_ami.ami-main-windows.id
+
+  iam_instance_profile    = aws_iam_instance_profile.main.name
+  disable_api_termination = true
+
+  user_data = templatefile("${path.module}/script/userdata.ps1", {
+    HOSTNAME = "CCPD01",
+    PASSWORD = var.PASSWORD
+  })
+  user_data_replace_on_change = true
+
+  subnet_id              = element(module.vpc-main.public_subnets, 1)
+  vpc_security_group_ids = [module.sg-main.security_group_id]
+
+}
+
+#
+# Golden Image (Main)
+#
+module "ec2-main-gi" {
+
+  providers = {
+    aws = aws.main
+  }
+
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.1"
+
+  name          = "GIPD01"
+  instance_type = "t3.medium"
+  ami           = data.aws_ami.ami-main-windows.id
+
+  iam_instance_profile    = aws_iam_instance_profile.main.name
+  disable_api_termination = true
+
+  user_data = templatefile("${path.module}/script/userdata.ps1", {
+    HOSTNAME = "GIPD01",
+    PASSWORD = var.PASSWORD
+  })
+  user_data_replace_on_change = true
+
+  subnet_id              = element(module.vpc-main.private_subnets, 0)
+  vpc_security_group_ids = [module.sg-main.security_group_id, module.sg-main-ssm.security_group_id]
+
+}
+
+#
+# Domain Controller (DR)
+#
 module "ec2-dr-dc" {
 
   providers = {
@@ -62,6 +128,36 @@ module "ec2-dr-dc" {
   user_data_replace_on_change = true
 
   subnet_id              = element(module.vpc-dr.public_subnets, 0)
+  vpc_security_group_ids = [module.sg-dr.security_group_id]
+
+}
+
+#
+# Cloud Connector (DR)
+#
+module "ec2-dr-cc" {
+
+  providers = {
+    aws = aws.dr
+  }
+
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.6.1"
+
+  name          = "CCPD51"
+  instance_type = "t3.medium"
+  ami           = data.aws_ami.ami-dr-windows.id
+
+  iam_instance_profile    = aws_iam_instance_profile.dr.name
+  disable_api_termination = true
+
+  user_data = templatefile("${path.module}/script/userdata.ps1", {
+    HOSTNAME = "CCPD51",
+    PASSWORD = var.PASSWORD
+  })
+  user_data_replace_on_change = true
+
+  subnet_id              = element(module.vpc-dr.public_subnets, 1)
   vpc_security_group_ids = [module.sg-dr.security_group_id]
 
 }
