04818998403200015d157f9a08a4eb59cd9c44d5
Ivan Botero <ivan.botero@protonmail.ch>
2024-08-18 18:17:26 -0500

    ✨Added scripts to set the DNS in both DC

--- scripts.tf
+++ scripts.tf
@@ -17,7 +17,8 @@ resource "local_file" "join-dr-dc" {
   filename = "${path.module}/script/1-join-dr-dc.ps1"
   content = templatefile("${path.module}/script/template/join.tpl", {
     HOSTNAME            = "DCPD51.${var.DOMAIN_NAME}",
-    SERVER_ADDRESS      = module.ec2-main-dc.private_ip,
+    SERVER_ADDRESS_1    = module.ec2-dr-dc.private_ip,
+    SERVER_ADDRESS_2    = module.ec2-main-dc.private_ip,
     DOMAIN_NAME         = var.DOMAIN_NAME,
     DOMAIN_NETBIOS_NAME = var.DOMAIN_NETBIOS_NAME,
     JOIN_USERNAME       = "Administrator",
@@ -37,8 +38,28 @@ resource "local_file" "deploy-dr-dc" {
   })
 }
 
+resource "local_file" "set-dns-dc-main" {
+  filename = "${path.module}/script/3-set-dns-dc-main.ps1"
+  content = templatefile("${path.module}/script/template/set-dns-dc.tpl", {
+    HOSTNAME         = "DCPD01.${var.DOMAIN_NAME}",
+    SERVER_ADDRESS_1 = module.ec2-main-dc.private_ip,
+    SERVER_ADDRESS_2 = module.ec2-dr-dc.private_ip,
+    DOMAIN_NAME      = var.DOMAIN_NAME
+  })
+}
+
+resource "local_file" "set-dns-dc-dr" {
+  filename = "${path.module}/script/4-set-dns-dc-dr.ps1"
+  content = templatefile("${path.module}/script/template/set-dns-dc.tpl", {
+    HOSTNAME         = "DCPD51.${var.DOMAIN_NAME}",
+    SERVER_ADDRESS_1 = module.ec2-dr-dc.private_ip,
+    SERVER_ADDRESS_2 = module.ec2-main-dc.private_ip,
+    DOMAIN_NAME      = var.DOMAIN_NAME
+  })
+}
+
 resource "local_file" "create-sites" {
-  filename = "${path.module}/script/3-create-sites.ps1"
+  filename = "${path.module}/script/5-create-sites.ps1"
   content = templatefile("${path.module}/script/template/create-sites.tpl", {
     HOSTNAME    = "DCPD01.${var.DOMAIN_NAME}",
     MAIN_SUBNET = module.vpc-main.vpc_cidr_block,
@@ -47,10 +68,11 @@ resource "local_file" "create-sites" {
 }
 
 resource "local_file" "join-main-cc" {
-  filename = "${path.module}/script/4-join-main-cc.ps1"
+  filename = "${path.module}/script/6-join-main-cc.ps1"
   content = templatefile("${path.module}/script/template/join.tpl", {
     HOSTNAME            = "CCPD01.${var.DOMAIN_NAME}",
-    SERVER_ADDRESS      = module.ec2-main-dc.private_ip,
+    SERVER_ADDRESS_1    = module.ec2-main-dc.private_ip,
+    SERVER_ADDRESS_2    = module.ec2-dr-dc.private_ip,
     DOMAIN_NAME         = var.DOMAIN_NAME,
     DOMAIN_NETBIOS_NAME = var.DOMAIN_NETBIOS_NAME,
     JOIN_USERNAME       = "Administrator",
@@ -59,10 +81,11 @@ resource "local_file" "join-main-cc" {
 }
 
 resource "local_file" "join-dr-cc" {
-  filename = "${path.module}/script/5-join-dr-cc.ps1"
+  filename = "${path.module}/script/7-join-dr-cc.ps1"
   content = templatefile("${path.module}/script/template/join.tpl", {
     HOSTNAME            = "CCPD51.${var.DOMAIN_NAME}",
-    SERVER_ADDRESS      = module.ec2-main-dc.private_ip,
+    SERVER_ADDRESS_1    = module.ec2-dr-dc.private_ip,
+    SERVER_ADDRESS_2    = module.ec2-main-dc.private_ip,
     DOMAIN_NAME         = var.DOMAIN_NAME,
     DOMAIN_NETBIOS_NAME = var.DOMAIN_NETBIOS_NAME,
     JOIN_USERNAME       = "Administrator",
@@ -71,10 +94,11 @@ resource "local_file" "join-dr-cc" {
 }
 
 resource "local_file" "join-main-gi" {
-  filename = "${path.module}/script/6-join-main-gi.ps1"
+  filename = "${path.module}/script/8-join-main-gi.ps1"
   content = templatefile("${path.module}/script/template/join.tpl", {
     HOSTNAME            = "GIPD01.${var.DOMAIN_NAME}",
-    SERVER_ADDRESS      = module.ec2-main-dc.private_ip,
+    SERVER_ADDRESS_1    = module.ec2-main-dc.private_ip,
+    SERVER_ADDRESS_2    = module.ec2-dr-dc.private_ip,
     DOMAIN_NAME         = var.DOMAIN_NAME,
     DOMAIN_NETBIOS_NAME = var.DOMAIN_NETBIOS_NAME,
     JOIN_USERNAME       = "Administrator",
