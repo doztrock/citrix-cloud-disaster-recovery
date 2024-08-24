76b2bbeaece5445731304ea095fddcc720a77bfa
Ivan Botero <ivan.botero@protonmail.ch>
2024-08-24 13:33:07 -0500

    🎨Splitted provider files

--- providers.tf
+++ providers.tf
@@ -13,17 +13,3 @@ terraform {
     }
   }
 }
-
-provider "aws" {
-  alias      = "main"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  region     = "us-east-1"
-}
-
-provider "aws" {
-  alias      = "dr"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-  region     = "us-west-2"
-}
