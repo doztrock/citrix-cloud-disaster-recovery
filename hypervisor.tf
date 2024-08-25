60e987ff4b5e845b14946b6538b184d085688e62
Ivan Botero <ivan.botero@protonmail.ch>
2024-08-25 18:49:56 -0500

    🚧Added Citrix AWS hypervisor

---
+++ hypervisor.tf
@@ -0,0 +1,8 @@
+# # AWS Hypervisor
+# resource "citrix_aws_hypervisor" "example-aws-hypervisor" {
+#   name       = "main-us-east-1"
+#   zone       = citrix_zone.main.id
+#   api_key    = var.MAIN_AWS_ACCESS_KEY_ID
+#   secret_key = var.MAIN_AWS_SECRET_ACCESS_KEY
+#   region     = "us-east-1"
+# }
