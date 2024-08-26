811560503b3d84cfbf6cffd3d0c78a93026df623
Ivan Botero <ivan.botero@protonmail.ch>
2024-08-25 22:35:22 -0500

    ⚡️Added NAT Gateway

--- vpc-dr.tf
+++ vpc-dr.tf
@@ -23,6 +23,10 @@ module "vpc-dr" {
   public_subnets  = [cidrsubnet(local.cidr-dr, 8, 1), cidrsubnet(local.cidr-dr, 8, 2)]
   private_subnets = [cidrsubnet(local.cidr-dr, 8, 101), cidrsubnet(local.cidr-dr, 8, 102)]
 
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
   enable_dns_hostnames    = true
   enable_dns_support      = true
   map_public_ip_on_launch = true
