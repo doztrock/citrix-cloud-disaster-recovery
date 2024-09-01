dde5149ff98f095786cd1c90c05dcc24a98fb297
Ivan Botero <ivan.botero@protonmail.ch>
2024-09-01 15:13:34 -0500

    🐛Added remaining outputs

---
+++ tgrunt/aws/terraform-modules/security-group/outputs.tf
@@ -0,0 +1,3 @@
+output "security_group_id" {
+  value = module.security-group.security_group_id
+}
--- tgrunt/aws/terraform-modules/vpc/outputs.tf
+++ tgrunt/aws/terraform-modules/vpc/outputs.tf
@@ -19,6 +19,14 @@ output "public_route_table_ids" {
   value = module.vpc.public_route_table_ids
 }
 
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
 output "vpc_cidr_block" {
   value = module.vpc.vpc_cidr_block
 }
